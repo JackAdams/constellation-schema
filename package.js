aa4afce33d9b37516352173f5a24dc2e95040002
Brent Abrahams <brent_abrahams@yahoo.com>
2017-06-27 17:09:21 +0800

    Update dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:schema',
-  version: '0.4.2',
+  version: '0.4.3',
   summary: 'Schema generator plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-schema.git',
   documentation: 'README.md',
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   api.use(['templating','blaze','underscore','ejson','reactive-var','reactive-dict','tracker'], 'client');
-  api.use('constellation:console@1.4.1', 'client');
+  api.use('constellation:console@1.4.2', 'client');
   api.use('babrahams:editable-json@0.6.4', 'client');
   api.use('juliancwirko:s-alert@3.1.4', 'client');
 
